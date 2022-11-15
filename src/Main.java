a97e9852a7cbe04ace81f731316a56cde60f17c2
Jack Law <jack.ck.law@pccw.com>
2022-11-15 15:01:37 +0800

    Check 1 layer with same directory

--- src/Main.java
+++ src/Main.java
@@ -3,34 +3,74 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.ArrayList;
 
 class Main {
-    private static final String OLD_VERSION_PATH = "old_version";
-    private static final String NEW_VERSION_PATH = "new_version";
+    private static final String OLD_VERSION_PATH = "please_put_the_old_version_here";
+    private static final String NEW_VERSION_PATH = "please_put_the_new_version_here";
 
     public static void main(String[] args) {
         List<String> old_ver_list = new ArrayList<>();
         getAllFile(old_ver_list, OLD_VERSION_PATH);
-        System.out.println(old_ver_list);
-        System.out.println("Files in the old_ver_list = "  + old_ver_list.size());
+        // System.out.println(old_ver_list);
+        // System.out.println("Files in the old_ver_list = " + old_ver_list.size());
 
         List<String> new_ver_list = new ArrayList<>();
         getAllFile(new_ver_list, NEW_VERSION_PATH);
-        System.out.println(new_ver_list);
-        System.out.println("Files in the new_ver_list = "  + new_ver_list.size());
+        // System.out.println(new_ver_list);
+        // System.out.println("Files in the new_ver_list = " + new_ver_list.size());
+
+        for (String n : new_ver_list) {
+            System.out.println(n);
+            String o = n.replace("please_put_the_new_version_here", "please_put_the_old_version_here");
+            System.out.println(o);
+            int compareResult;
+
+            try {
+                compareResult = filesCompareByLine(o, n);
+            } catch (IOException e) {
+                compareResult = 0;
+                e.printStackTrace();
+            }
+
+            switch (compareResult) {
+                case 1:
+                    // code block
+                    System.out.println("Files are same" + "\n");
+                    break;
+                case 2:
+                    // code block
+                    System.out.println("Files are not same" + "\n");
+                    break;
+                case 3:
+                    // code block
+                    System.out.println("Added new file" + "\n");
+                    break;
+                case 4:
+                    // code block
+                    System.out.println("Deleted old file" + "\n");
+                    break;
+                default:
+                    // code block
+                    System.out.println("compareResult error");
+            }
+        }
 
     }
 
     /**
      * Compare two files
-     * if same, output -1
-     * if not, output lineNumber
+     * @param output = 1, same
+     * @param output = 2, not same
+     * @param output = 3, added
+     * @param output = 4, deleted
+     * @param lineNumber to store the different line, not used in this stage
      */
-    public static long filesCompareByLine(Path path1, Path path2) throws IOException {
-        // Same file, output -1
-        // Not same file, output lineNumber
+    public static int filesCompareByLine(String o, String n) throws IOException {
+        Path path1 = Paths.get(o);
+        Path path2 = Paths.get(n);
         try (BufferedReader bf1 = Files.newBufferedReader(path1);
                 BufferedReader bf2 = Files.newBufferedReader(path2)) {
             long lineNumber = 1;
@@ -38,14 +78,14 @@ class Main {
             while ((line1 = bf1.readLine()) != null) {
                 line2 = bf2.readLine();
                 if (line2 == null || !line1.equals(line2)) {
-                    return lineNumber;
+                    return 2;
                 }
                 lineNumber++;
             }
             if (bf2.readLine() == null) {
-                return -1;
+                return 1;
             } else {
-                return lineNumber;
+                return 2;
             }
         }
     }
@@ -64,7 +104,7 @@ class Main {
                 if (!item.getName().contains(".DS_Store")) {
                     list.add(item.getPath());
                     // System.out.println(item.getPath());
-                }                            
+                }
             } else if (item.isDirectory()) {
                 File[] items = item.listFiles();
                 for (File f : items) {
