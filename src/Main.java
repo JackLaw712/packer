e3acf14c1fee59e659af297773d2ecc0e82ca244
Jack Law <jack.ck.law@pccw.com>
2022-11-18 16:14:52 +0800

    log

--- src/Main.java
+++ src/Main.java
@@ -76,7 +76,7 @@ class Main {
      * @param text the log message
      */
     public static void log(String text) {
-        try (FileWriter f = new FileWriter("package_log.log", true);
+        try (FileWriter f = new FileWriter(PACKAGE_PATH + "//package_log.log", true);
                 BufferedWriter b = new BufferedWriter(f);
                 PrintWriter p = new PrintWriter(b);) {
             p.print(getCurrentDateTime() + "\n");
